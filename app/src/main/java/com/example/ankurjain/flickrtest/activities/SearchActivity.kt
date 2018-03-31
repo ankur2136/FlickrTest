f413fea8ada2713cd7b5ae830b89448845ab4097
Ankur Jain <ankurjain@yahoo-inc.com>
2018-03-31 13:02:24 -0700

    Added http logging.

--- app/src/main/java/com/example/ankurjain/flickrtest/activities/SearchActivity.kt
+++ app/src/main/java/com/example/ankurjain/flickrtest/activities/SearchActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.widget.ProgressBar
 import com.example.ankurjain.flickrtest.R
 import com.example.ankurjain.flickrtest.adapters.SearchAdapter
@@ -11,11 +12,16 @@ import com.example.ankurjain.flickrtest.dto.GalleryItem
 import com.example.ankurjain.flickrtest.network.FlickrAPI
 import com.example.ankurjain.flickrtest.network.ListWrapper
 import com.google.gson.GsonBuilder
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+
 
 class SearchActivity : AppCompatActivity() {
 
@@ -23,14 +29,20 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var flickrAPI: FlickrAPI
 
+    companion object {
+        const val TAG = "SearchActivity"
+    }
+
     private val queryCallback = object : Callback<ListWrapper<GalleryItem>> {
         override fun onFailure(call: Call<ListWrapper<GalleryItem>>?, t: Throwable?) {
+            Log.d(TAG, "failed to call $call")
         }
 
         override fun onResponse(call: Call<ListWrapper<GalleryItem>>?, response: Response<ListWrapper<GalleryItem>>?) {
+            Log.d(TAG, response?.body().toString())
             if (response != null) {
                 if (response.isSuccessful) {
-                    val items = response.body()?.items
+                    val items = response.body()?.photo
                     val adapter = recyclerView.adapter as SearchAdapter
                     adapter.appendItems(items)
                 }
@@ -52,7 +64,13 @@ class SearchActivity : AppCompatActivity() {
 
     private fun createApi() {
         val gson = GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").create()
-        val retroFit = Retrofit.Builder().baseUrl(FlickrAPI.BASE_URL).addConverterFactory(GsonConverterFactory.create(gson)).build()
+
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val httpClient = OkHttpClient.Builder()
+        httpClient.interceptors().add(logging)
+
+        val retroFit = Retrofit.Builder().baseUrl(FlickrAPI.BASE_URL).addConverterFactory(GsonConverterFactory.create(gson)).client(httpClient.build()).build()
         flickrAPI = retroFit.create(FlickrAPI::class.java)
     }
 }
--- app/src/main/java/com/example/ankurjain/flickrtest/network/ListWrapper.java
+++ app/src/main/java/com/example/ankurjain/flickrtest/network/ListWrapper.java
@@ -3,9 +3,5 @@ package com.example.ankurjain.flickrtest.network;
 import java.util.List;
 
 public class ListWrapper<T> {
-    private List<T> items;
-
-    public List<T> getItems() {
-        return items;
-    }
+    public List<T> photo;
 }
